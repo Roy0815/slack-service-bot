8c9abaa9d04f0483d5bb01cbd8bddc42a4762472
Roy Lotzwik <19552716+Roy0815@users.noreply.github.com>
2025-08-10 18:10:58 +0200

    Cleanup webhook handler

--- src/signatures/webhooks.js
+++ src/signatures/webhooks.js
@@ -1,5 +1,7 @@
-import * as controller from './webhook-controller.js';
 import * as awsRtAPI from '../general/aws-runtime-api.js';
+import { userJoiningFields } from '../general/masterdata/types.js';
+
+import fetch from 'node-fetch';
 
 // Handle webhook events from docuseal
 /** @type {import('aws-lambda').LambdaFunctionURLHandler } */
@@ -29,7 +31,7 @@ export async function handler(event, context) {
         await awsRtAPI.sendResponse();
 
         // Handle form completed event
-        await controller.handleFormCompleted(event);
+        await handleFormCompleted(event);
         break;
 
       default:
@@ -61,4 +63,53 @@ export async function handler(event, context) {
       body: JSON.stringify({ error: 'Internal server error' })
     };
   }
+}
+
+/**
+ * @param {import('aws-lambda').APIGatewayProxyEventV2} event
+ */
+export async function handleFormCompleted(event) {
+  const submission = JSON.parse(event.body);
+
+  // check correct template
+  if (
+    !submission.data ||
+    !submission.data.template ||
+    `${submission.data.template.id}` !==
+      process.env.APPLICATION_FORM_TEMPLATE_ID
+  ) {
+    console.log('Template not handled', submission);
+    return;
+  }
+
+  // check values exist
+  if (!submission.data.values) {
+    console.log('No values in submission', submission);
+    return;
+  }
+
+  /** @type {import('../general/masterdata/types.js').userJoiningDetails} */
+  const applicationInfo = {};
+
+  // Extract values from the submission
+  submission.data.values.forEach((value) => {
+    if (!userJoiningFields.includes(value.field)) return;
+
+    if (/(joinedDate|birthday|signingDate)/.test(value.field)) {
+      // Convert date fields
+      applicationInfo[value.field] = value.value.split('-').reverse().join('.');
+      return;
+    }
+
+    applicationInfo[value.field] = value.value;
+  });
+
+  // set file url
+  applicationInfo.docusealFileURL = submission.data.documents?.[0]?.url;
+
+  // start slack workflow
+  await fetch(process.env.SIGNATURE_WORKFLOW_SLACK_WEBHOOK, {
+    method: 'post',
+    body: JSON.stringify(applicationInfo)
+  });
 }
