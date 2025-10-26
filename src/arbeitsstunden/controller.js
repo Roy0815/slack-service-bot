7cb68df6dfe01fabe44331d6475e027d90b58f59
Roy Lotzwik <19552716+Roy0815@users.noreply.github.com>
2025-10-26 13:17:24 +0100

    Grammatik Korrektur

--- src/arbeitsstunden/controller.js
+++ src/arbeitsstunden/controller.js
@@ -278,7 +278,7 @@ export function getUserRegisterEndMessage({ slackId, name, approved }) {
     channel: slackId,
     text: `Deine Registrierung als ${name} wurde ${
       approved
-        ? 'genehmigt. Du kannst jetzt deine Arbeitsstunden, Stammdaten erfassen und abrufen und dich für Wettkämpfe melden.'
+        ? 'genehmigt. Du kannst jetzt deine Arbeitsstunden erfassen, deine Stammdaten ändern und dich für Wettkämpfe melden.'
         : 'abgelehnt.'
     }`
   };
